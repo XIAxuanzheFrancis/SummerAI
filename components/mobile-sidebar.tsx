49fe576255418a94ed3f5203cad93ca4f29983f0
Xuanzhe XIA <xuanzhe.xia@etu.uca.fr>
2024-04-05 06:38:10 +0200

    mobile sidebar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Menu } from 'lucide-react';
+
+import { Button } from "@/components/ui/button";
+import {Sheet, SheetTrigger, SheetContent} from "@/components/ui/sheet";
+import SideBar from "@/components/sidebar";
+
+const MobileSidebar = () => {
+    return(
+        <>
+            <Sheet>
+                <SheetTrigger>
+                    <Button variant="ghost" size="icon" className="md:hidden">
+                        <Menu />
+                    </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="p-0">
+                    <SideBar/>
+                </SheetContent>
+            </Sheet>
+        </>
+    );
+}
+
+export default MobileSidebar;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 const Navbar = () => {
   return (
     <>
       <div className="flex items-center p-4">
-        <Button variant="ghost" size="icon" className="md:hidden">
-          <Menu />
-        </Button>
-      </div>
-      <div className="flex w-full justify-end p-4">
-        <UserButton afterSignOutUrl="/" />
+          <MobileSidebar/>
+        <div className="flex w-full justify-end p-4">
+            <UserButton afterSignOutUrl="/" />
+        </div>
       </div>
     </>
   );
