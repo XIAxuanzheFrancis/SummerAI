81f6d363799b99f31cdbd140c7beb537072974aa
Xuanzhe XIA <xuanzhe.xia@etu.uca.fr>
2024-04-05 11:44:33 +0200

    Complete all dashboard details

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,12 +3,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import { MessageSquare } from "lucide-react";
+import { ArrowRight, MessageSquare } from "lucide-react";
 import { ImageIcon } from "lucide-react";
 import { VideoIcon } from "lucide-react";
 import { Music } from "lucide-react";
 import { Code } from "lucide-react";
 import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const tools = [
   {
@@ -49,6 +51,7 @@ const tools = [
 ];
 
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <>
       <div className="mb-8 space-y-4">
@@ -62,8 +65,22 @@ const DashboardPage = () => {
 
       <div className="px-4 md-px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
-          <Card key={tool.href} className="p-4 border-black/10 flex items-center justify-between hover:shadow-md transition cursor-pointer">
-            
+          <Card
+            onClick={() => {
+            router.push(tool.href);
+            }}
+            key={tool.href}
+            className="p-4 border-black/10 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold text-sm">
+                {tool.label}
+              </div>
+            </div>
+            <ArrowRight className="w-6 h-6"/>
           </Card>
         ))}
       </div>
